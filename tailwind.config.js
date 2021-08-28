dc85241512e1184b9ee7fbbca1ae4bbb32a91f2f
Axel de la Torre <axeldelat@gmail.com>
2021-08-28 15:29:30 -0500

    remove purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  purge: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
