7b09fe1753d9d150918c9fe8794d8d77050ecdd9
Axel de la Torre <axeldelat@gmail.com>
2021-09-16 13:07:08 -0500

    tailwind purge fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: false,
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
