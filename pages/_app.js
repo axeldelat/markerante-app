f4ff2be93cf147dbd086ce95b63d7cdc9f5e7eda
Axel de la Torre <axeldelat@gmail.com>
2021-09-17 20:35:28 -0500

    Test FB Pixel

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,23 @@
 import '../styles/global.css'
 import WhatsAppWidget from 'react-whatsapp-widget'
 import 'react-whatsapp-widget/dist/index.css'
+import ReactPixel from 'react-facebook-pixel'
 
 function MyApp({ Component, pageProps }) {
+  const advancedMatching = { em: 'email@email.com' }; // optional, more info: https://developers.facebook.com/docs/facebook-pixel/advanced/advanced-matching
+  const options = {
+  autoConfig: true, // set pixel's autoConfig. More info: https://developers.facebook.com/docs/facebook-pixel/advanced/
+  debug: false, // enable logs
+  };
+
+  ReactPixel.init('984742198976988', advancedMatching, options);
+
+  ReactPixel.pageView(); // For tracking page view
+  ReactPixel.track(event, data); // For tracking default events. More info about standard events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#standard-events
+  ReactPixel.trackSingle('PixelId', event, data); // For tracking default events.
+  ReactPixel.trackCustom(event, data); // For tracking custom events. More info about custom events: https://developers.facebook.com/docs/facebook-pixel/implementation/conversion-tracking#custom-events
+  ReactPixel.trackSingleCustom('PixelId', event, data); // For tracking custom events.
+
   return (
     <>
       <Component {...pageProps} />
