480bec82ce5a5dc45cc316f4a4225d0e2f294f26
Axel de la Torre <axeldelat@gmail.com>
2021-09-20 20:45:59 -0500

    404 page added

--- pages/_app.js
+++ pages/_app.js
@@ -4,12 +4,10 @@ import FacebookPixel from '../components/analitycs/FacebookPixel'
 
 function MyApp({ Component, pageProps }) {
   return (
-    
       <FacebookPixel>
         <Component {...pageProps} />
         <WhatsAppWidget phoneNumber='525626202797' textReplyTime='Respuesta inmediata' message='Hola! 👋🏼 ¿Cómo podemos ayudarte?' companyName='Markerante' sendButton='Enviar'/>
       </FacebookPixel>
-    
   )
 }
 
