3cce98ef328757aac4ba34874c3c9fecac8e8de0
Axel de la Torre <axeldelat@gmail.com>
2022-07-10 08:17:02 -0500

    Query logos from sanity

--- components/footer/RestaurantCard.js
+++ components/footer/RestaurantCard.js
@@ -4,15 +4,16 @@ import { urlFor } from "../../lib/sanity"
 const RestaurantCard = ({ restaurant }) => {
   const theurl = urlFor(restaurant)
   return (
-    <div key={restaurant._id} className="col-span-1 flex justify-center bg-white rounded-lg">
-      <Link href={restaurant.restaurantWebsite} passHref>
-        <a target="_blank">
+    <Link href={restaurant.restaurantWebsite} passHref>
+      <a target="_blank">
+        <div key={restaurant._id} className="col-span-1 flex justify-center bg-white rounded-lg">
           <img
           src={urlFor(restaurant.restaurantLogo)}
+          alt={restaurant.restaurantName}
           />
-        </a>
-      </Link>
-    </div>
+        </div>
+      </a>
+    </Link>
   )
 }
 
